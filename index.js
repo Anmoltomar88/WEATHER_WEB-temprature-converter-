ecd7175fcb7e1113ed899a1432dee90c56fea188
Anmol <tomaranmol513@gmail.com>
2023-11-02 15:21:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var inputvalue = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var humidity = document.querySelector('#humidity')
+apik ="63a90ae96d390ec37d6c1252f5a86e1a"
+function convertion(val)
+{
+    return (val - 273).toFixed(3)
+}
+
+btn.addEventListener('click', function()
+{
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
+  .then(res => res.json())
+
+  .then(data => 
+  {
+    var nameval = data['name']
+    var descrip = data['weather']['0']['description']
+    var tempature = data['main']['temp']
+    var wndspeed = data['wind']['speed']
+    var Humidity = data['main']['humidity']
+
+
+    city.innerHTML=`Weather of <span>${nameval}<span>`
+    temp.innerHTML = `Temperature: <span>${ convertion(tempature)} C</span>`
+    description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
+    wind.innerHTML = `Wind Speed: <span>${wndspeed} km/h<span>`
+    humidity.innerHTML = `Humidity as: <span>${Humidity}<span>`
+  })
+
+  .catch(err => alert('You entered Wrong city name'))
+})
